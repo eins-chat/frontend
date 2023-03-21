24022bf425667373b4716ffa69572996d3586b7b
Ralph-1006 <ralphboehm2002@yahoo.de>
2023-03-21 17:50:08 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  message = 'eine nachricht';
+  sender = 'a';
+  receiver = 'b';
+  title = 'chat';
+  socket: WebSocket | null = null;
+
+  connect() {
+    this.socket = new WebSocket('ws://localhost:8080');
+    this.socket.onopen = () => {
+      console.log('[open] Connection established');
+      console.log('Sending to server');
+      let registerMessage = {
+        sender: this.sender,
+      };
+      if (this.socket) this.socket.send(JSON.stringify(registerMessage));
+    };
+    this.socket.onmessage = function (event) {
+      console.log(`[message] Data received from server: ${event.data}`);
+    };
+
+    this.socket.onclose = function (event) {
+      if (event.wasClean) {
+        console.log(
+          `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+        );
+      } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('[close] Connection died');
+      }
+    };
+
+    this.socket.onerror = function (error) {
+      console.log(`[error]`);
+    };
+  }
+  sendMessage() {
+    const message = {
+      receiver: this.receiver,
+      message: this.message,
+    };
+    if (this.socket) this.socket.send(JSON.stringify(message));
+  }
+}
